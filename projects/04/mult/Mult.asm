b9aba1e27f5c964d2a2be3ca6f8de93c042877b3
multifrench <multifrench@protonmail.com>
2021-07-26 21:50:30 +0300

    Implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,44 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@R1
+D = M
+@i
+M = D
+// i = RAM[1]
+
+@result
+M = 0
+// result = 0
+
+(LOOP)
+@i
+D = M
+@LOOP_END
+D; JLE
+// end if i <= 0
+
+@R0
+D = M
+@result
+M = M + D
+// result += RAM[0]
+
+@i
+M = M - 1
+// i -= 1
+
+@LOOP
+0; JMP
+
+(LOOP_END)
+
+@result
+D = M
+@R2
+M = D
+// RAM[2] = result
+
+(EXIT)
+@EXIT
+0; JMP
